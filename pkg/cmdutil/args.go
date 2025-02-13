8e75fe81077591d19961a3c76d270879e0f9f4d1
Tyler McGoffin <jtmcg@github.com>
2025-02-13 15:30:38 -0800

    Add testing to GlobWindowsPaths

--- pkg/cmdutil/args.go
+++ pkg/cmdutil/args.go
@@ -3,7 +3,6 @@ package cmdutil
 import (
 	"fmt"
 	"path/filepath"
-	"runtime"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -61,20 +60,17 @@ func NoArgsQuoteReminder(cmd *cobra.Command, args []string) error {
 }
 
 func GlobWindowsPaths(patterns []string) ([]string, error) {
-	if runtime.GOOS != "windows" {
-		return patterns, nil
-	}
-	var expansions []string
+	expansions := []string{}
+
 	for _, pattern := range patterns {
 		matches, err := filepath.Glob(pattern)
 		if err != nil {
-			return nil, fmt.Errorf("%s: %s", pattern, err)
+			return nil, fmt.Errorf("%s: %v", pattern, err)
 		}
-		if len(matches) > 0 {
+		if len(matches) != 0 {
 			expansions = append(expansions, matches...)
-		} else {
-			expansions = append(expansions, pattern)
 		}
 	}
+
 	return expansions, nil
 }
